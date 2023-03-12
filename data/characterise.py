3591dbf4f0d63766fe01097f13939002db87222c
hamhaf <hamzahhafejee@gmail.com>
2023-03-12 17:50:15 +0000

    get csvs of cwd

--- data/characterise.py
+++ data/characterise.py
@@ -4,6 +4,15 @@
 import pandas as pd
 import os
 
-# for each csv file, read it in and output some statistics 
-csvs = [] # list of the csvs
+#get csvs of cwd
+def get_csvs():
+    cwd = os.getcwd()
+    csvs = [] # list of the csvs
+    for file in os.listdir(cwd):
+        if file.endswith(".csv"):
+            csvs.append(file)
+    return csvs
 
+print(get_csvs())
+
+# for each csv file, read it in and output some statistics 

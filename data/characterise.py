e7a023dca7fee1dffc33c649aec590b71f1843cd
hamhaf <hamzahhafejee@gmail.com>
2023-03-12 18:07:10 +0000

    code to perform initial check that the train and test sets match in cols and rows - output redirected to characterise.txt

--- data/characterise.py
+++ data/characterise.py
@@ -13,6 +13,13 @@ def get_csvs():
             csvs.append(file)
     return csvs
 
-print(get_csvs())
+# print(get_csvs())
 
 # for each csv file, read it in and output some statistics 
+def characterise():
+    csvs = get_csvs()
+    for file in csvs:
+        df = pd.read_csv(file)
+        print(f"{file}: \nrows: {df.shape[0]} \ncols: {df.shape[1]}")
+
+characterise()

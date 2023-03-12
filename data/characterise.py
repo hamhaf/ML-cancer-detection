3b6a4435f1cb2307d5af0b455e6fd0a6930ca2af
hamhaf <hamzahhafejee@gmail.com>
2023-03-12 17:43:38 +0000

    data characterisation - pseudocode

---
+++ data/characterise.py
@@ -0,0 +1,9 @@
+# python file to characterise the dataset
+
+# import libraries
+import pandas as pd
+import os
+
+# for each csv file, read it in and output some statistics 
+csvs = [] # list of the csvs
+
